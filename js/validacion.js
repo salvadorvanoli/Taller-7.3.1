fb3144b814797ff32fec6e48c43fcef9f58f3b5f
salvadorvanoli <141060855+salvadorvanoli@users.noreply.github.com>
2023-08-09 19:14:03 -0300

    Cambios

--- js/validacion.js
+++ js/validacion.js
@@ -1,3 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+let submitButton = document.getElementById("regBtn");
+
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
 }
@@ -5,3 +9,22 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+function validarFormulario(){
+    let password = document.getElementById("password1");
+    let scndpassword = document.getElementById("password2");
+    let nombre = document.getElementById("nombre");
+    let apellido = document.getElementById("apellido");
+    let email = document.getElementById("email");
+    let check = document.getElementById("terminos");
+    if(!password || !scndpassword || password.textContent!=scndpassword.textContent){
+        showAlertError();
+    } else {
+        showAlertSuccess();
+    }
+}
+
+// || password.length>6) || (!scndpassword || scndpassword!=password) || !nombre || !apellido || !email || !check){
+
+submitButton.addEventListener("click", validarFormulario);
+});
