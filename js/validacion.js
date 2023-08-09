8aff3e6333f9971604b719eb06eee912ce317a02
salvadorvanoli <141060855+salvadorvanoli@users.noreply.github.com>
2023-08-09 19:37:56 -0300

    Solución final

--- js/validacion.js
+++ js/validacion.js
@@ -1,30 +1,31 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-let submitButton = document.getElementById("regBtn");
-
-function showAlertSuccess() {
-    document.getElementById("alert-success").classList.add("show");
-}
-
-function showAlertError() {
-    document.getElementById("alert-danger").classList.add("show");
-}
-
-function validarFormulario(){
+    let form = document.getElementsByTagName("form")[0];
+    let submitButton = document.getElementById("regBtn");
     let password = document.getElementById("password1");
     let scndpassword = document.getElementById("password2");
     let nombre = document.getElementById("nombre");
     let apellido = document.getElementById("apellido");
     let email = document.getElementById("email");
     let check = document.getElementById("terminos");
-    if(!password || !scndpassword || password.textContent!=scndpassword.textContent){
-        showAlertError();
-    } else {
-        showAlertSuccess();
+
+    function showAlertSuccess() {
+        document.getElementById("alert-success").classList.add("show");
     }
-}
 
-// || password.length>6) || (!scndpassword || scndpassword!=password) || !nombre || !apellido || !email || !check){
+    function showAlertError() {
+        document.getElementById("alert-danger").classList.add("show");
+    }
 
-submitButton.addEventListener("click", validarFormulario);
+    function validarFormulario(event){
+        // Evita que la página se recargue
+        event.preventDefault();
+        if(nombre.value && apellido.value && email.value && password.value===scndpassword.value && password.value.length>=6 && scndpassword.value.length>=6 && check.checked){
+            showAlertSuccess();
+        }else{
+            showAlertError();
+        }
+    }
+
+    form.addEventListener("submit", validarFormulario);
 });
